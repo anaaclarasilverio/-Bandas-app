f45a7c4a5bed86c4bbf5b80a2f7ce961a883a7b0
anaaclarasilverio <ds.46277728822@gmail.com>
2024-04-04 09:05:41 -0300

    Link do Projeto atualizado (Rotas)

---
+++ src/screens/musicDetails.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function MusicDetails() {
+    return(
+        <View>
+            <Text>Detalhes da música</Text>
+        </View>
+    );
+}
