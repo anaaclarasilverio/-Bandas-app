203b26233202c95032ef4d30e8a58709dd4e64dd
anaaclarasilverio <ds.46277728822@gmail.com>
2024-04-11 10:43:37 -0300

    conclusão do projeto

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,53 +1,68 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, StatusBar, FlatList } from "react-native";
+import { Audio } from "expo-av";
 import MusicItem from "../components/MusicItem";
 
 export default function Home({ navigation }) {
   const [currentPlaying, setCurrentPlaying] = useState(null);
+  const [currentSound, setCurrentSound] = useState(null);
   const [musicData, setMusicData] = useState([]);
-  const item = {
-    id: 1,
-    title: "Os anjos cantam",
-    group: "Jorge e Mateus",
-    album_image: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/9ee32724242445.563319ce9cdc2.jpg",
-    year: 2010,
-    genre: "Sertanejo",
+
+  const togglePlayPause = async (item) => {
+    if (currentSound && currentPlaying == item.id) {
+      await currentSound.pauseAsync();
+      setCurrentPlaying(null);
+      setCurrentSound(null);
+    } else {
+      if (currentSound) {
+        await currentSound.unloadAsync();
+      }
+      const { sound } = await Audio.Sound.createAsync(
+        { uri: `http://10.0.2.2:3000/musics/${item.music_path}` },
+        {shouldPlay: true}
+      );
+      setCurrentSound(sound);
+      setCurrentPlaying(item.id);
+
+    }
   };
 
   useEffect(() => {
     fetch("http://10.0.2.2:3000/musics")
       .then((response) => response.json())
       .then((data) => setMusicData(data));
-  }, [])
+  }, []);
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor="#121212" />
-      <Text style={styles.title}>Minhas músicas</Text>
-      <FlatList data={musicData} keyExtractor={(item) => item.id.toString()}
+      <Text style={styles.title}>Minhas Músicas</Text>
+
+      <FlatList
+        data={musicData}
+        keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
           <MusicItem
-            isPlaying={() => currentPlaying == item.id}
+            isPlaying={currentPlaying == item.id}
             music={item}
             navigation={navigation}
-            onPlayPause={() => { }}
-          />
-
+            onPlayPause={() => togglePlayPause(item)} />
         )} />
-
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#121212",
-    padding: 16,
+    paddingTop: 16,
   },
   title: {
     fontSize: 36,
     fontWeight: "bold",
     color: "#fff",
-    marginTop: 20,
+    marginBottom: 20,
     marginLeft: 20,
   },
 })
+
